b75a76378c28f1fba58aeeeb26a1d470a4f88911
JuanSilva <juangusilva99@gmail.com>
2022-05-18 22:04:38 -0300

    exclude SharedPreferences in "SobreActivity"

--- app/src/main/java/com/example/loginpi5s/Activities/SobreActivity.java
+++ app/src/main/java/com/example/loginpi5s/Activities/SobreActivity.java
@@ -1,29 +1,53 @@
 package com.example.loginpi5s.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.loginpi5s.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SobreActivity extends AppCompatActivity {
 
     //declaração de variáveis
     private TextView userInfoTxt;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
-        SharedPreferences dados = getSharedPreferences("Dados", MODE_PRIVATE);
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
         //chama o método que sincrorniza as views
         IniciarComponentes();
 
         //coloca o nome do usuário no topo da tela
-        userInfoTxt.setText(dados.getString("nome", "nome"));
+        UpdateName();
+    }
+
+    //pega o nome do usuario no firebase
+    private void UpdateName() {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("Usuarios").child(currentUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userInfoTxt.setText(snapshot.child("nome").getValue(String.class));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     //Sincronizar botões Java com XML
